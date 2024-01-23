922203aa8ea28bbce1d659f7ce277118c3ac15fd
marvi3 <marvi3@gmx.net>
2024-01-23 13:20:00 +0100

    Create xls.py

---
+++ xls.py
@@ -0,0 +1,19 @@
+# This script requires the installation of xlwt, openpyxl and xlrd
+
+import pandas as pd
+import openpyxl
+
+# A method that modifies only one cell
+# The row and col start at 1
+def modifyCell(row, col, file, data):
+  df.read_excel(file)
+  df.iloc[row, col] = data
+  df.to_excel(file, index=False, columns=False)
+
+# A method that writes a two-dimensional array to an excel sheet.
+# It might overwrite existing excel sheets
+# The row and col start at 1
+def writeExcelFile(file, data):
+  df = pd.DataFrame(data)
+  df.to_excel(file)
+
