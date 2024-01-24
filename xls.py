1bc131c6b6f5f2d861fa6250838d5926a3c8f6c2
marvi3 <marvi3@gmx.net>
2024-01-24 15:32:22 +0100

    Update xls.py

--- xls.py
+++ xls.py
@@ -6,28 +6,28 @@ import openpyxl
 # A method that modifies only one cell
 # The row and col start at 0
 def modifyCell(row, col, file, data):
-  df = pd.read_excel(file)
-  df.iloc[row, col] = data.strip()
-  df.to_excel(file, index=False, columns=False)
+    df = pd.read_excel(file)
+    df.iloc[row, col] = data.strip()
+    df.to_excel(file, index=False, columns=False)
 
 # A method that modifies one row of an excel sheet
 # The row starts at 0
 def modifyRow(row, file, data):
-  try:
-    df = pd.read_excel(file)
-  except:
-   df = pd.DataFrame(data)
-  for i in range(len(data)):
-    df.iloc[row, i] = data[i].strip()
-  df.to_excel(file, index=False, columns=False)
+    try:
+        df = pd.read_excel(file)
+    for i in range(len(data)):
+        df.iloc[row, i] = data[i].strip()
+    except:
+        df = pd.DataFrame(data)
+    df.to_excel(file, index=False, columns=False)
 
 # A method that writes a two-dimensional array to an excel sheet.
 # It might overwrite existing excel sheets
 # The row and col start at 1
 def writeExcelFile(file, data):
-  df = pd.DataFrame(data)
-  df.to_excel(file)
+    df = pd.DataFrame(data)
+    df.to_excel(file)
 
 def writeExcelFile(file, data, rowNames, columnNames):
-  df = pd.DataFrame(data, index=rowNames, columns=columnNames)
-  df.to_excel(file)
+    df = pd.DataFrame(data, index=rowNames, columns=columnNames)
+    df.to_excel(file)
