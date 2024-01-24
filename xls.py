175d9502f00a75969f73b959e5817d661789c54e
marvi3 <marvi3@gmx.net>
2024-01-24 18:20:12 +0100

    test

--- xls.py
+++ xls.py
@@ -42,7 +42,7 @@ def add_strings_to_row(row_index, file_name, strings):
 # The row starts at 0
 def modifyRow(row, file, data):
     try:
-        df = pd.read_csv(file)
+        df = pd.read_csv(file, header=0)
     except FileNotFoundError:
         print("being in excpetion")
         df = pd.DataFrame()
