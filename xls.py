bdd5ce5fad1f3b4ab514586611a439b9659cdad7
marvi3 <marvi3@gmx.net>
2024-01-24 18:18:57 +0100

    test

--- xls.py
+++ xls.py
@@ -47,7 +47,7 @@ def modifyRow(row, file, data):
         print("being in excpetion")
         df = pd.DataFrame()
     
-    print("The df is \n", df)
+    print("The df of row", row, "is \n", df)
 
     print("The rows existing before are", len(df))
     if row >= len(df):
