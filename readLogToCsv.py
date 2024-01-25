ba25fd5975dfc9c6a9e570c9fef48ab95cef5cbe
marvi3 <marvi3@gmx.net>
2024-01-25 10:53:56 +0100

    fixed a bug

--- readLogToCsv.py
+++ readLogToCsv.py
@@ -103,7 +103,7 @@ if int(sys.argv[3]) == 0:
             entryResult = parseString.getSubstringLengthList(logEntryString, startStringList, stringLengthList, occList, True, cutFromBeginningList)
             splitTime += time.time() - partStartTime
             partStartTime = time.time()
-            df = xls.modifyRow(entryNumber, df, entryResult, entryNumber + reportEveryRounds + )
+            df = xls.modifyRow(entryNumber, df, entryResult, entryNumber + reportEveryRounds - entryNumber % reportEveryRounds)
             writeTime += time.time() - partStartTime
             entryNumber += 1
             if entryNumber % reportEveryRounds == 0:
