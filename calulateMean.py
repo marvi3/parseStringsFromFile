6eb24c305d9c2eff19e7787ed22f0f9e4eb1e9c3
marvi3 <marvi3@gmx.net>
2024-02-05 18:53:23 +0100

    Working version of the getStartEndRows method

--- calulateMean.py
+++ calulateMean.py
@@ -2,95 +2,204 @@ import pandas as pd
 import sys
 import xls
 from datetime import datetime, timedelta
+import locale
+import time
 
 
-def calculate_end_date(start_date_str, seconds):
+
+def calculateEndDateStr(startDateStr, seconds):
     # Convert the input start_date_str to a datetime object
-    start_date = datetime.strptime(start_date_str, '%d.%m.%Y %H:%M')
+    startDate = datetime.strptime(startDateStr, '%d.%m.%Y %H:%M')
 
     # Calculate the end date by adding the specified number of seconds
-    end_date = start_date + timedelta(seconds=seconds)
+    endDate = startDate + timedelta(seconds=seconds)
 
     # Return the end date as a string
-    return end_date.strftime('%d.%m.%Y %H:%M')
+    return endDate.strftime('%d.%m.%Y %H:%M')
 
-def getStartEndRows(weekFile="weekForRun.csv", coresFile="cpuCureForRun.csv", startsFile="startingTime.csv", runningFile="runningTime.csv", cpuRun1File="CPUUserPerRun1.csv", cpuRun2File="CPUUserPerRun2.csv", cpuRun3File="CPUUserPerRun3.csv"):
-    weeks = xls.readCsv(weekFile)
-    cores = xls.readCsv(coresFile)
+
+
+def calculateEndDate(startDate, seconds):
+    # Calculate the end date by adding the specified number of seconds
+    endDate = (startDate + timedelta(seconds=int(seconds))).replace(second=0, microsecond=0)
+
+    # Return the end date
+    return endDate
+
+
+
+def convertDate(dateString):
+    # print(dateString)
+    strMod = dateString.split(" ")
+    strMod = strMod[1:4] + strMod[5:6]
+    strMod = ' '.join(strMod)
+
+    formatString = "%d. %b %H:%M:%S %Y"
+    locale.setlocale(locale.LC_TIME, 'de_DE.UTF-8')
+
+    return datetime.strptime(strMod, formatString).replace(second=0, microsecond=0)
+
+
+
+def init2DArray(rows, columns, initialValue):
+    return [[initialValue for i in range(columns)] for j in range(rows)]
+
+
+def getStartEndRows(runPeriods="runPeriods.csv", coresFile="cpuCoreForRun.csv", startsFile="startingTime.csv", runningFile="runningTime.csv", cpuRun1File="CPUUserPerRun1.csv", cpuRun2File="CPUUserPerRun2.csv", cpuRun3File="CPUUserPerRun3.csv"):
+    runPeriods = xls.readCsv(runPeriods)
+    # cores = xls.readCsv(coresFile)
     starts = xls.readCsv(startsFile)
     running = xls.readCsv(runningFile)
     cpuRun1 = xls.readCsv(cpuRun1File)
     cpuRun2 = xls.readCsv(cpuRun2File)
     cpuRun3 = xls.readCsv(cpuRun3File)
-    numRuns = len(weeks.columns) - 2
-    numSutFuzzer = len(weeks) - 1
-    startRows = [[0] * numRuns] * numSutFuzzer
-    startWeeks = [[0] * numRuns] * numSutFuzzer
-    endRows = [[0] * numRuns] * numSutFuzzer
-    endWeeks = [[0] * numRuns] * numSutFuzzer
+    numRuns = len(starts.columns) - 2
+    numSutFuzzer = len(starts) - 1
+    startRows = init2DArray(numSutFuzzer, numRuns, 0)
+    startWeeks = init2DArray(numSutFuzzer, numRuns, 0)
+    endRows = init2DArray(numSutFuzzer, numRuns, 0)
+    endWeeks = init2DArray(numSutFuzzer, numRuns, 0)
 
     for row in range(1,numSutFuzzer + 1):
         for column in range(2,numRuns + 2):
-            week = weeks.iloc[row, column].split(",")
-            if len(week) == 1:
-                weekStart = week[0]
-                weekEnd = week[0]
-            elif len(week) == 2:
-                weekStart = week[0]
-                weekEnd = week[1]
-            else:
-                print("A week-field can not have less than one or more than two entries but", row, column, "did.")
-                sys.exit(1)
-            core = cores.iloc[row,column]
-            start = starts.iloc[row,column]
+            if(type(starts.iloc[row,column]) != str or starts.iloc[row,column] == "WIEDERH"):
+                continue
+            startDate = datetime.strptime(starts.iloc[row,column], '%d.%m.%Y %H:%M')
             duration = running.iloc[row, column]
-            end = calculate_end_date([start, duration])
-            if int(weekStart) == 1:
-                for row in range(0, len(cpuRun1)):
-                    if str(cpuRun1.iloc[row, 0]).startswith(start):
-                        startWeeks[row, column] = weekStart
-                        startRows[row, column] = row
-                        break
-            elif int(weekStart) == 2:
-                for row in range(0, len(cpuRun2)):
-                    if str(cpuRun2.iloc[row, 0]).startswith(start):
-                        startWeeks[row, column] = weekStart
-                        startRows[row, column] = row
-                        break
-            elif int(weekStart) == 3:
-                for row in range(0, len(cpuRun3)):
-                    if str(cpuRun3.iloc[row, 0]).startswith(start):
-                        startWeeks[row, column] = weekStart
-                        startRows[row, column] = row
-                        break
+            endDate = calculateEndDate(startDate, duration)
+            # core = cores.iloc[row,column]
+            weekStart = 0
+            weekEnd = 0
+            for period in range(1,len(runPeriods.columns)):
+                startPeriod = datetime.strptime(runPeriods.iloc[1, period], '%d.%m.%Y %H:%M')
+                endPeriod = datetime.strptime(runPeriods.iloc[2, period], '%d.%m.%Y %H:%M')
+                if startDate >= startPeriod and startDate <= endPeriod:
+                    weekStart = period
+                    break
+                # elif startDate < datetime.strptime(runPeriods.iloc[1, 1], '%d.%m.%Y %H:%M'):
+                #     weekStart = period - 0.5
+                #     break
+                # elif startDate < startPeriod and startDate < datetime.strptime(runPeriods.iloc[2, period - 1], '%d.%m.%Y %H:%M'):
+                #     weekEnd = period - 0.5
+            for period in range(1,len(runPeriods.columns)):
+                startPeriod = datetime.strptime(runPeriods.iloc[1, period], '%d.%m.%Y %H:%M')
+                endPeriod = datetime.strptime(runPeriods.iloc[2, period], '%d.%m.%Y %H:%M')
+                if endDate >= startPeriod and endDate <= endPeriod:
+                    weekEnd = period
+                    break
+                elif period == len(runPeriods.columns) - 1:
+                    weekEnd = float(period) + 0.5
+                    break
+                elif endDate > startPeriod and endDate < datetime.strptime(runPeriods.iloc[1, period + 1], '%d.%m.%Y %H:%M'):
+                    weekEnd = float(period) + 0.5
+                    break
+            if weekStart == 0:
+                print("The startdate", startDate, "of column", column, "and row", row, "doesn't seem to be within the recorded period")
+                if weekEnd == 0:
+                    print("The enddate", endDate, "of column", column, "and row", row, "doesn't seem to be within the recorded period")
+                    sys.exit(1)
+                sys.exit(1)
+            if weekEnd == 0:
+                print("The enddate", endDate, "of column", column, "and row", row, "doesn't seem to be within the recorded period")
+                sys.exit(1)
             
-            if int(weekEnd) == 1:
-                for row in range(len(cpuRun1), 0):
-                    if str(cpuRun1.iloc[row, 0]).startswith(end):
-                        endWeeks[row, column] = weekEnd
-                        endRows[row, column] = row
-                        break
-            elif int(weekEnd) == 2:
-                for row in range(len(cpuRun2), 0):
-                    if str(cpuRun2.iloc[row, 0]).startswith(end):
-                        endWeeks[row, column] = weekEnd
-                        endRows[row, column] = row
-                        break
-            elif int(weekEnd) == 3:
-                for row in range(len(cpuRun3), 0):
-                    if str(cpuRun3.iloc[row, 0]).startswith(end):
-                        endWeeks[row, column] = weekEnd
-                        endRows[row, column] = row
-                        break
+            continueWhile = True
+            if weekStart == 1:
+                timeRow = round((startDate - convertDate(cpuRun1.iloc[1, 0])).total_seconds() / 11) + 1
+                while continueWhile:
+                    if ((startDate - convertDate(cpuRun1.iloc[timeRow, 0])).total_seconds()) < 0:
+                        timeRow += round((startDate - convertDate(cpuRun1.iloc[timeRow, 0])).total_seconds() / 9)
+                    elif ((startDate - convertDate(cpuRun1.iloc[timeRow, 0])).total_seconds()) < 1000:
+                        for timeRow in range(timeRow, len(cpuRun1)):
+                            if convertDate(cpuRun1.iloc[timeRow, 0]) == startDate:
+                                startWeeks[row - 1][column - 2] = weekStart
+                                startRows[row - 1][column - 2] = timeRow
+                                continueWhile = False
+                                break
+                    else:
+                        timeRow += round((startDate - convertDate(cpuRun1.iloc[timeRow, 0])).total_seconds() / 11)
+            elif weekStart == 2:
+                timeRow = round((startDate - convertDate(cpuRun2.iloc[1, 0])).total_seconds() / 11) + 1
+                while continueWhile:
+                    if ((startDate - convertDate(cpuRun2.iloc[timeRow, 0])).total_seconds()) < 0:
+                        timeRow += round((startDate - convertDate(cpuRun2.iloc[timeRow, 0])).total_seconds() / 9)
+                    elif ((startDate - convertDate(cpuRun2.iloc[timeRow, 0])).total_seconds()) < 1000:
+                        for timeRow in range(timeRow, len(cpuRun2)):
+                            if convertDate(cpuRun2.iloc[timeRow, 0]) == startDate:
+                                startWeeks[row - 1][column - 2] = weekStart
+                                startRows[row - 1][column - 2] = timeRow
+                                continueWhile = False
+                                break
+                    else:
+                        timeRow += round((startDate - convertDate(cpuRun2.iloc[timeRow, 0])).total_seconds() / 11)
+            elif weekStart == 3:
+                timeRow = round((startDate - convertDate(cpuRun3.iloc[1, 0])).total_seconds() / 11) + 1
+                while continueWhile:
+                    if ((startDate - convertDate(cpuRun3.iloc[timeRow, 0])).total_seconds()) < 0:
+                        timeRow += round((startDate - convertDate(cpuRun3.iloc[timeRow, 0])).total_seconds() / 9)
+                    elif ((startDate - convertDate(cpuRun3.iloc[timeRow, 0])).total_seconds()) < 1000:
+                        for timeRow in range(timeRow, len(cpuRun3)):
+                            if convertDate(cpuRun3.iloc[timeRow, 0]) == startDate:
+                                startWeeks[row - 1][column - 2] = weekStart
+                                startRows[row - 1][column - 2] = timeRow
+                                continueWhile = False
+                                break
+                    else:
+                        timeRow += round((startDate - convertDate(cpuRun3.iloc[timeRow, 0])).total_seconds() / 11)
+            
+            continueWhile = True
+            if weekEnd == 1:
+                timeRow = round((endDate - convertDate(cpuRun1.iloc[1, 0])).total_seconds() / 11) + 1
+                while continueWhile:
+                    if ((endDate - convertDate(cpuRun1.iloc[timeRow, 0])).total_seconds()) < 0:
+                        timeRow += round((endDate - convertDate(cpuRun1.iloc[timeRow, 0])).total_seconds() / 9)
+                    elif ((endDate - convertDate(cpuRun1.iloc[timeRow, 0])).total_seconds()) < 1000:
+                        for timeRow in range(timeRow, len(cpuRun1)):
+                            if convertDate(cpuRun1.iloc[timeRow, 0]) == endDate:
+                                endWeeks[row - 1][column - 2] = weekEnd
+                                endRows[row - 1][column - 2] = timeRow
+                                continueWhile = False
+                                break
+                    else:
+                        timeRow += round((endDate - convertDate(cpuRun1.iloc[timeRow, 0])).total_seconds() / 11)
+            elif weekEnd == 2:
+                timeRow = round((endDate - convertDate(cpuRun2.iloc[1, 0])).total_seconds() / 11) + 1
+                while continueWhile:
+                    if ((endDate - convertDate(cpuRun2.iloc[timeRow, 0])).total_seconds()) < 0:
+                        timeRow += round((endDate - convertDate(cpuRun2.iloc[timeRow, 0])).total_seconds() / 9)
+                    elif ((endDate - convertDate(cpuRun2.iloc[timeRow, 0])).total_seconds()) < 1000:
+                        for timeRow in range(timeRow, len(cpuRun2)):
+                            if convertDate(cpuRun2.iloc[timeRow, 0]) == endDate:
+                                endWeeks[row - 1][column - 2] = weekEnd
+                                endRows[row - 1][column - 2] = timeRow
+                                continueWhile = False
+                                break
+                    else:
+                        timeRow += round((endDate - convertDate(cpuRun2.iloc[timeRow, 0])).total_seconds() / 11)
+            elif weekEnd == 3:
+                timeRow = round((endDate - convertDate(cpuRun3.iloc[1, 0])).total_seconds() / 11) + 1
+                while continueWhile:
+                    if ((endDate - convertDate(cpuRun3.iloc[timeRow, 0])).total_seconds()) < 0:
+                        timeRow += round((endDate - convertDate(cpuRun3.iloc[timeRow, 0])).total_seconds() / 9)
+                    elif ((endDate - convertDate(cpuRun3.iloc[timeRow, 0])).total_seconds()) < 1000:
+                        for timeRow in range(timeRow, len(cpuRun3)):
+                            if convertDate(cpuRun3.iloc[timeRow, 0]) == endDate:
+                                endWeeks[row - 1][column - 2] = weekEnd
+                                endRows[row - 1][column - 2] = timeRow
+                                continueWhile = False
+                                break
+                    else:
+                        timeRow += round((endDate - convertDate(cpuRun3.iloc[timeRow, 0])).total_seconds() / 11)
+            elif weekEnd == 3.5:
+                endWeeks[row - 1][column - 2] = weekEnd
+                endRows[row - 1][column - 2] = len(cpuRun3) - 2
+                # print("The enddate", endDate, "of column", column, "and row", row, "doesn't seem to be within the recorded period. The enddate has been set to", convertDate(cpuRun3.iloc[endRows[row - 1][column - 2], 0]))
     
     return [startRows, startWeeks, endRows, endWeeks]
 
 
 
-
-
-
-def calculateAveragesColumns(inputFiles, outputFile, startRows=0, endRows=0):
+def calculateAveragesColumns(inputFile, outputFile, startRows=0, endRows=0, startPeriod=0, endPeriod=0):
     df = pd.read_csv(inputFile)
     numColumns = len(df.columns)
     if startRows == 0 and endRows == 0:
@@ -128,11 +237,19 @@ def calculateAveragesColumns(inputFiles, outputFile, startRows=0, endRows=0):
     xls.createCsvFile(outputFile, averageDf)
 
 
+
 if __name__ == "__main__":
     # stuff only to run when not called via 'import' here
     startRows, startWeeks, endRows, endWeeks = getStartEndRows()
-    if len(sys.argv) < 4:
-        print("Usage: python calculateMean.py csvToBeRead.csv(if multiple separate with comma) csvToWrite.csv c/r(for column or row)")
-        sys.exit(0)
-    if sys.argv[3] == "c":
-        calculateAveragesColumns(sys.argv[1].split(","),sys.argv[2])
+    # for i in range(0, len(startRows)):
+    #     print("\n\n\nrow", i + 1)
+    #     print(startRows[i])
+    #     print(startWeeks[i])
+    #     print(endRows[i])
+    #     print(endWeeks[i])
+
+    # if len(sys.argv) < 4:
+    #     print("Usage: python calculateMean.py csvToBeRead.csv csvToWrite.csv c/r(for column or row)")
+    #     sys.exit(0)
+    # if sys.argv[3] == "c":
+    #     calculateAveragesColumns(sys.argv[1],sys.argv[2])
